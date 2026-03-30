5f7c8d9cb71d49ed4822213997dc9e290c72d959
aidenbg <aiden.buragohain@gmail.com>
2026-03-30 00:00:54 -0700

    Added introduction to lab_1a

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Aiden" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hi everyone! My name is Aiden, and I'm a junior from California. I like to fence (the sport), read, cook, go on runs, and eat good food. I'm interested in how Computer Science and specifically AI can be applied to the real world, and I'm excited to meet everyone!")
 if __name__ == "__main__":
     main()
